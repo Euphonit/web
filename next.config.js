21df2d258b00737d5c79c7504d0d293c9f74d01e
Euphonit <dancrutti@gmail.com>
2025-02-03 10:30:21 -0600

    test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: "/web",
-  images: {
-    unoptimized: true, // GitHub Pages does not support Next.js image optimization
-  },
-};
+const nextConfig = {};
 
-module.exports = nextConfig;
-
+export default nextConfig;
