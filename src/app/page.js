478a9fd7b83ecb71fee39db7e22aadf6a788eed7
Euphonit <dancrutti@gmail.com>
2025-02-02 19:30:24 -0600

    im stupid

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import * as Text from "./comp/text";
 export default function HomePage() {
   return (
     <div>
-      <NavButton content="Blog" local="/Blog" />
+      <NavButton content="Blog" local="./Blog" />
       <Text.H1 content="Im Daniel Crutti (euphonit) and this is my website!" />
       <Text.H4 content="Im a highschool student" />
       <Text.P content="Some info about me is that I love all things music and theater!" />
