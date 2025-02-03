5d298b39085b278d0b9516080bc87048a8090f53
Euphonit <dancrutti@gmail.com>
2025-02-02 19:44:25 -0600

    shit what

--- app/page.js
+++ app/page.js
@@ -6,7 +6,6 @@ export default function HomePage() {
     <div>
       <NavButton content="Blog" local="/Blog" />
       <Text.H1 content="Im Daniel Crutti (euphonit) and this is my website!" />
-      <Text.H4 content="Im a highschool student" />
       <Text.P content="Some info about me is that I love all things music and theater!" />
     </div>
   );
