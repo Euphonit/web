35ecb4763a7d6c2ead56eadb2dd02527b139d815
Euphonit <dancrutti@gmail.com>
2025-02-22 13:32:35 -0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,13 @@ import * as font from "./components/font";
 export default function Home() {
   return (
     <div className="">
+      <p
+        className={`${font.poppins.variable} antialiased text-lg bg-blue-500 text-white px-4 py-2 rounded-md font-medium cursor-pointer hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors duration-200 active:bg-blue-700 // Optional active state`}
+      >
+        <Link href="/pages/blog">Click Here to go to my blog</Link>
+      </p>
       <h1 className={`${font.poppins.variable} antialiased text-4xl`}>
-        <Link href="/pages/blog">Test</Link>
+        Welcome to my website at euphonit.uk
       </h1>
     </div>
   );
--- src/app/pages/blog/page.js
+++ src/app/pages/blog/page.js
@@ -1,3 +1,24 @@
+import * as font from "../../components/font";
+import Link from "next/link";
+
 export default function () {
-  return <h1 className="text-center">Hi</h1>;
+  return (
+    <div className="">
+      <p
+        className={`${font.poppins.variable} antialiased text-2xl bg-blue-500 px-4 py-2 rounded-md cursor-pointer hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors duration-200 active:bg-blue-700 // Optional active state`}
+      >
+        <Link href="../..">Click Here to go to go Home</Link>
+      </p>
+      <h1
+        className={`${font.poppins.variable} antialiased text-4xl text-center bg-red-500`}
+      >
+        Blogs
+      </h1>
+      <p
+        className={`${font.poppins.variable} antialiased text-2xl bg-green-700 rounded-md active:bg-blue-700 transition-colors duration-200 font-light px-4 py-2 cursor-pointer hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2`}
+      >
+        <Link href="./blogs/journey">1# My journey thus far</Link>
+      </p>
+    </div>
+  );
 }
