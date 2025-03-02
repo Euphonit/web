408604fd2e290d380af321a158c3950ed65e653f
Euphonit <dancrutti@gmail.com>
2025-03-02 16:01:57 -0600

    I hope this works

--- src/app/pages/hunter/page.js
+++ src/app/pages/hunter/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="">
       <p
-        className={`${font.poppins.variable} antialiased text-lg bg-blue-500 text-white px-4 py-2 rounded-md font-medium cursor-pointer hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors duration-200 active:bg-blue-700 // Optional active state`}
+        className={`${font.poppins.variable} antialiased text-lg bg-orange-500 text-white px-4 py-2 rounded-md font-medium cursor-pointer hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors duration-200 active:bg-orange-700 // Optional active state`}
       >
         <Link href="/pages/blog">Click Hunter to hunter to hunter blog</Link>
       </p>
