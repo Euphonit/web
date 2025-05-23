03620d1b3a66d91380511d9094c39e18e5463653
Euphonit <dancrutti@gmail.com>
2025-05-22 20:22:39 -0700

    Very small tidy up

--- src/app/pages/hunter/page.js
+++ src/app/pages/hunter/page.js
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <div className="">
       <p
-        className={`${font.poppins.variable} antialiased text-lg bg-orange-500 text-white px-4 py-2 rounded-md font-medium cursor-pointer hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors duration-200 active:bg-orange-700 // Optional active state`}
+        className={`--font-poppins antialiased text-lg bg-orange-500 text-white px-4 py-2 rounded-md font-medium cursor-pointer hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors duration-200 active:bg-orange-700 // Optional active state`}
       >
         <Link href="/pages/blog">Hunter Hunter to go to Hunter blog</Link>
       </p>
-      <h1 className={`${font.poppins.variable} antialiased text-clip text-4xl`}>
+      <h1 className={`--font-poppins antialiased text-clip text-4xl`}>
         Hunter, i'm Hunter Crutti (Hunter) and Hunter is Hunter website!
         <br /> Hunter of Hunter:
       </h1>
