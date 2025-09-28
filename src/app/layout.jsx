a8f782e59c4dbfe21a9870e04b46f68c73397fcd
euphonit <dancrutti@gmail.com>
2025-09-28 00:06:36 -0500

    analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import "./components/font";
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
       <body className="--font-poppins bg-blue-950 text-white">
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
