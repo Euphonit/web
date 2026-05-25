6728571a72ed4dc9686c0b521fafa79da10ed34d
euphonit <dancrutti@gmail.com>
2026-05-25 15:52:33 -0500

    dep update + nav modification

--- src/app/components/mainnav.jsx
+++ src/app/components/mainnav.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function MainNav({ type }) {
   if (type === "home") {
     return (
-      <div className="grid grid-cols-3 gap-x-1 mx-1 xs:text-xs sm:text-sm lg:text-lg xl:text-2xl">
+      <div className="grid grid-cols-2 gap-x-1 mx-1 xs:text-xs sm:text-sm lg:text-lg xl:text-2xl">
         <Link href="/pages/blog">
           <button className="antialiased bg-blue-500 text-white px-4 py-2 mt-1 rounded-3xl font-medium cursor-pointer hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors duration-200 active:bg-blue-700 w-full">
             📝 Blog
@@ -12,15 +12,6 @@ export default function MainNav({ type }) {
         <Link href="/pages/photo">
           <button className="antialiased bg-orange-500 text-white px-4 py-2 mt-1 rounded-3xl font-medium cursor-pointer hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 transition-colors duration-200 active:bg-orange-700 w-full">
             📷 Photo
-          </button>
-        </Link>
-        <Link
-          href="https://fs.danielcrutti.com/cam/vid%20%2B%20sound/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="antialiased bg-red-500 text-white px-4 py-2 mt-1 rounded-3xl font-medium cursor-pointer hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition-colors duration-200 active:bg-red-700 w-full">
-            🎬 Videos ➡️ 🔑
           </button>
         </Link>
       </div>
